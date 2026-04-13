88a254f45d81e38c9557ba0f87f2b35e5a88e1d3
Jhonatan Paschoal <jhtn.paschoal@gmail.com>
2026-04-13 14:00:08 -0300

    feat: adiciona Google Tag Manager e ajusta Pixel no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,35 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${gate.variable} ${poppins.variable}`}>
       <body className={`font-sans antialiased`}>
+        
+        {/* --- GOOGLE TAG MANAGER (NOSCRIPT - BODY) --- */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WKGCDDLH"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* -------------------------------------------- */}
+
+        {/* --- GOOGLE TAG MANAGER (SCRIPT - HEAD) --- */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WKGCDDLH');
+            `,
+          }}
+        />
+        {/* -------------------------------------------- */}
+
+        {/* Meta Pixel - Apenas Inicialização */}
         <Script
           id="fb-pixel"
           strategy="afterInteractive"
@@ -103,6 +132,8 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        {/* -------------------------------------------- */}
+
         {children}
         <Analytics />
       </body>
