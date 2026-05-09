fbc9e37bbef1f19574c0f8284d7994bab15d9414
Techweniu <tecnologia@weniu.com.br>
2026-05-09 19:28:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,23 @@ export default function RootLayout({
         />
         {/* -------------------------------------------- */}
 
+        {/* --- GOOGLE ANALYTICS (gtag.js) --- */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-521XPTZ9BB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-521XPTZ9BB');
+          `}
+        </Script>
+        {/* -------------------------------------------- */}
+
         {children}
         <Analytics />
       </body>
