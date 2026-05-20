ba807678a80597468f72d8fc9529307787ca0d4c
Jhonatan Paschoal <jhtn.paschoal@gmail.com>
2026-05-20 10:33:14 -0300

    remove botão flutuante do whatsapp

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <ResultsCarousel />
       <Pricing onSelectPlan={setSelectedPlan} />
       <FinalCTA />
-      <FloatingWhatsapp />
+     
     </main>
   )
 }
